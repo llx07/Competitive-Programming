38b134a04734ba9241b197b8e0a979c8d866aa2f
Lexiao Lin <2623459403@qq.com>
2024-09-17 16:40:53 +0800

    1

--- algo/数据结构/树状数组.cpp
+++ algo/数据结构/树状数组.cpp
@@ -2,7 +2,7 @@ template<typename T = int >
 struct BIT{
     int n;
     vector<T> t;
-    BIT(int _n):n(_n),t(_n+2){}
+    BIT(int _n):n(_n+1),t(_n+2){}
     void add(int i,T x){i++;for(;i<=n;i+=i&-i)t[i]+=x;}
     void add(int l,int r,T x){add(l,x);add(r+1,-x);}
     T query(int i){i++;T da=0;for(;i;i-=i&-i)da+=t[i];return da;}
