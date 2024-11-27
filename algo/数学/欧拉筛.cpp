7362ba35374eef5e30da030851a18fda6b2558a6
Lexiao Lin <2623459403@qq.com>
2024-11-27 20:27:05 +0800

    1

--- algo/数学/欧拉筛.cpp
+++ algo/数学/欧拉筛.cpp
@@ -1,13 +1,13 @@
 int p[N/2],cnt;
 bool vis[N];
 
-void init(int n){
+void init(){
     vis[1]=1;
-    for(int i=2;i<=n;i++){
+    for(int i=2;i<N;i++){
         if(!vis[i]){
             p[++cnt]=i;
         }
-        for(int j=1;j<=cnt&&i*p[j]<=n;j++){
+        for(int j=1;j<=cnt&&i*p[j]<N;j++){
             vis[i*p[j]]=1;
             if(i%p[j]==0)break;
         }
