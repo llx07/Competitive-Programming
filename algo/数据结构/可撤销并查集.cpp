649695246cdd2f6edc838defabb9a8dd639a96ed
Lexiao Lin <2623459403@qq.com>
2025-06-21 13:31:57 +0800

    update 可撤销并查集.cpp

--- algo/数据结构/可撤销并查集.cpp
+++ algo/数据结构/可撤销并查集.cpp
@@ -1,28 +1,24 @@
 struct Revdsu{
     int fa[N],sz[N];
-    vector<pair<int&,int>> hisfa,hissz;
+    vector<pair<int&,int>> his;
     void init(int n){
         for(int i=1;i<=n;i++)fa[i]=i,sz[i]=1;
-        hisfa.clear();hissz.clear();
+        his.clear();
     }
     int find(int x){
         while(x!=fa[x])x=fa[x];
         return x;
     }
     void unite(int u,int v){
-        debug(u,v);
         u=find(u),v=find(v);
         if(u==v)return;
         if(sz[u]<sz[v])swap(u,v);
-        hisfa.push_back({fa[v],fa[v]});
-        hissz.push_back({sz[u],sz[u]});
+        his.push_back({fa[v],fa[v]});
+        his.push_back({sz[u],sz[u]});
         fa[v]=u;sz[u]+=sz[v];
     }
-    int S(){return hisfa.size();}
+    int S(){return his.size();}
     void rev(int S){
-        while(hisfa.size()>S){
-            auto [a1,a2]=hisfa.back();hisfa.pop_back();a1=a2;
-            auto [b1,b2]=hissz.back();hissz.pop_back();b1=b2;
-        }
+        while(his.size()>S){auto [a1,a2]=his.back();his.pop_back();a1=a2;}
     }
-}p;
+}dsu;
